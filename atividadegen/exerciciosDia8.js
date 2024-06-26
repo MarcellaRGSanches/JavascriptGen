c5612ba34445639ee82195fe2c9218eec42c7e24
vinicius_menendez <vinnymnascimento@hotmail.com>
2024-06-26 12:27:00 -0300

    apaguei alguns dos comentários

--- atividadegen/exerciciosDia8.js
+++ atividadegen/exerciciosDia8.js
@@ -2,10 +2,8 @@
 const leia = require('readline-sync')
 let salario;
 salario = leia.questionInt("Digite o valor do seu salario: ")
-// console.log( + salario);
 let abono; 
 abono = leia.questionInt("Digite o valor do seu abono: ")
-// console.log("Escreva o seu abono: " + abono);
 let novoSalario = salario + abono;
 console.log("Então o seu novo salario é:" + novoSalario)
 // -------------------------------------
